cdb51283daa96df474a415827578d2044c6d4d3a
MohammadFaysal731 <www.faysal731@gmail.com>
2022-05-23 23:42:08 +0600

    create pruchase api

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 require('dotenv').config()
@@ -24,6 +24,12 @@ async function run() {
             const cursor = partCollection.find(query);
             const parts = await cursor.toArray();
             res.send(parts);
+        });
+        app.get('/purchase/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const purchase = await partCollection.findOne(query);
+            res.send(purchase);
         })
 
     }
