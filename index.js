49b49d8a8c8ffa3256c1d48823faed05e2aa79e3
MohammadFaysal731 <www.faysal731@gmail.com>
2022-05-27 22:09:12 +0600

    create delver api

--- index.js
+++ index.js
@@ -21,20 +21,21 @@ async function run() {
         const userCollection = client.db('manufacturer_factory').collection('users');
         const orderCollection = client.db('manufacturer_factory').collection('orders');
         const reviewCollection = client.db('manufacturer_factory').collection('reviews');
-
+        // this api for all parts
         app.get('/part', async (req, res) => {
             const query = {};
             const cursor = partCollection.find(query);
             const parts = await cursor.toArray();
             res.send(parts);
         });
+        // this api for specific part
         app.get('/part/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
             const part = await partCollection.findOne(query);
             res.send(part);
         });
-
+        // this api for crete all user and saved the database  
         app.put('/user/:email', async (req, res) => {
             const email = req.params.email;
             const user = req.body;
@@ -50,40 +51,45 @@ async function run() {
         app.get('/orders', async (req, res) => {
             const orders = await orderCollection.find().toArray();
             res.send(orders);
-        })
+        });
+        // this api for deliver orders
+        app.delete('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const deliver = await orderCollection.deleteOne(query);
+            res.send(deliver);
+        });
 
-
-        // this api for specific order 
+        // this api for specific order per email 
         app.get('/order', async (req, res) => {
             const email = req.query.email;
             const query = { email: email };
             const order = await orderCollection.find(query).toArray();
             res.send(order);
-        })
+        });
 
-
+        // this api for create order
         app.post('/order', async (req, res) => {
             const orders = req.body;
             const query = { name: orders.name, email: orders.email, address: orders.address, phone: orders.phone, productName: orders.productName, productQuantity: orders.productQuantity, }
             const order = await orderCollection.insertOne(query);
             res.send(order);
         });
-
+        // this api for delete specific order
         app.delete('/order/:id', async (req, res) => {
             const id = req.params;
             const query = { _id: ObjectId(id) }
             const result = await orderCollection.deleteOne(query);
             res.send(result);
-        })
-
-
+        });
+        // this api for get all review
         app.get('/review', async (req, res) => {
             const query = {};
             const cursor = reviewCollection.find(query);
             const review = await cursor.toArray();
             res.send(review);
         });
-
+        // this api for create review
         app.post('/review', async (req, res) => {
             const reviews = req.body;
             const query = { name: reviews.name, email: reviews.email, rating: reviews.rating, description: reviews.description, }
